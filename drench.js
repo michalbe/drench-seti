831591f764b4975e32ec0fe20778aa3bfafa13d2
Michał Budzyński <michal@virtualdesign.pl>
2015-10-01 12:41:52 +0200

    bruteforce

--- drench.js
+++ drench.js
@@ -5,17 +5,26 @@ var sizeY = 8;
 var sizeCell = 35;
 var board = [];
 var moves = 0;
+var cachedBoard = {};
 
-var generateBoard = function(){
-	for (var j = 0; j < sizeX; j++) {
-		board[j] = [];
-	  for (var i = 0; i < sizeY; i++) {
-			board[j][i] = ~~(Math.random()*7);
-		}
-	}
-};
+var board = [
+	[1,3,0,2,1,0,0,0],
+	[0,3,1,1,2,2,1,0],
+	[3,1,3,4,3,1,1,2],
+	[0,3,4,3,3,3,2,1],
+	[0,6,5,3,1,4,5,1],
+	[5,0,6,5,4,1,4,0],
+	[0,6,5,6,5,1,3,1],
+	[0,0,0,1,0,3,6,0],
+];
 
+// var board = [
+// 	[0, 0, 0],
+// 	[0, 3, 3],
+// 	[3, 3, 3]
+// ];
 
+cachedBoard = JSON.parse(JSON.stringify(board));
 var colors = [
 	'rgb(255,255,255)',
 	'rgb(254,202,8)',
@@ -27,6 +36,7 @@ var colors = [
 ];
 
 function draw () {
+	return;
   var canvas = document.getElementById('canvas');
 	canvas.width = sizeX * sizeCell;
 	canvas.height = sizeY * sizeCell;
@@ -51,15 +61,18 @@ function setUpClickHandlers () {
 }
 
 
-function destColor (evt) {
+function destColor (color) {
 	moves++;
-	document.querySelector('#moves span').textContent = moves;
+	color = parseInt(color, 10);
+	//document.querySelector('#moves span').textContent = moves;
 	checkedSquares = [];
 	borderBlocks = {};
-	var color = evt.target.getAttribute('data-color');
-	checkElement(0, 0, checkColor(0, 0), parseInt(color, 10));
-	draw();
-	console.log(borderBlocks);
+	//var color = evt.target.getAttribute('data-color');
+	checkElement(0, 0, checkColor(0, 0), color);
+	// checkedSquares = [];
+	// borderBlocks = {};
+	// checkElement(0, 0, checkColor(0, 0), color);
+	//draw();
 }
 
 function checkWinningConditions(){
@@ -117,13 +130,6 @@ function checkElement(x, y, sourceColor, destColor) {
 			// console.log('S');
 			checkElement(x, y+1, sourceColor, destColor);
 		}
-	} else {
-		var blockColor = checkColor(x, y);
-		if (blockColor in borderBlocks) {
-			borderBlocks[blockColor]++;
-		} else {
-			borderBlocks[blockColor] = 1;
-		}
 	}
 }
 
@@ -133,42 +139,153 @@ function updateElement(x, y, color) {
 	board[y][x] = color;
 }
 
-generateBoard();
-setUpClickHandlers();
-draw();
+// setUpClickHandlers();
+// draw();
 var lowest = 9999;
 var highest = 0;
-var games = 0;
+var games = 23000000;
+var currentMove = 0;
+var maxMoves = 16;
+var maxSequence = '0400000000000000';
 
-var checkMostPopularNeighbour = function(){
-
+var makeSeq = function(seq){
+	seq = seq.toString();
+	while(seq.length < maxMoves) {
+		seq = '0' + seq;
+	}
+	return seq;
 };
 
-var timeout;
-var autoPlay = function(){
-	var buttons = document.querySelectorAll('button');
-	var button = buttons[~~(Math.random()*buttons.length)];
-	button.click();
-	if (!checkWinningConditions()) {
-		timeout = setTimeout(autoPlay, 1);
-	} else {
-		games++;
-		if (moves < lowest) {
-			lowest = moves;
-			moves = '<span style="color: #f00">' + moves + '</span>';
-			console.log('Lowest:', lowest);
-		} else if (moves > highest) {
-			highest = moves;
-			moves = '<span style="color: #00f">' + moves + '</span>';
-			console.log('Highest:', highest);
-		}
-		document.querySelector('#moves div').textContent = games;
+var nextSeq = function(seq) {
+	seq = parseInt(seq, 7);
+	seq++;
+	return makeSeq(seq.toString(7));
+};
 
-		document.getElementById('history').innerHTML += moves + ', ';
-		document.getElementById('history').scrollTop = 100000;
-		moves = 0;
-		generateBoard();
-		draw();
+var sequence = '0301016052462130';
+
+var reg = /(.)\1/i;
+var timeout;
+var go = function(){
+	var threads = 80;
+	while(threads--) {
 		autoPlay();
 	}
 };
+
+var gps = 0;
+var hcform = document.getElementById('hc-form');
+var hcmsg = document.getElementById('hc-msg');
+var sendmsg = true;
+var sendHCMsg = function(msg){
+	hcmsg.value = msg ||
+							  'Games: ' 	 				 + games +
+							'. Sequence: ' 				 + sequence +
+							'. Games per second: ' + gps;
+
+	hcform.submit();
+	if (sendmsg) {
+		setTimeout(sendHCMsg, 1000 * 60 * 5);
+	}
+};
+
+setTimeout(sendHCMsg, 10000);
+
+var autoPlay = function() {
+	// var mostPopCol;
+	// var qty = 0;
+	//var button;
+	//var buttons = document.querySelectorAll('button');
+	//
+	// if ((Object.keys(borderBlocks).length > 0) && (Math.random() > 0.5)) {
+	// 	for (var block in borderBlocks) {
+	// 		if (borderBlocks[block] > qty) {
+	// 			mostPopCol = block.replace('c', '');
+	// 		}
+	// 	}
+	//
+	// 	button = buttons[mostPopCol];
+	// } else {
+	// 	button = buttons[~~(Math.random()*buttons.length)];
+	// }
+
+	destColor(sequence[currentMove]);
+
+	if (!checkWinningConditions()) {
+		if (currentMove !== maxMoves-1) {
+			// jestem w trakcie gry
+			timeout = setTimeout(autoPlay, 1);
+			currentMove++;
+		} else {
+			// zmaksowalem gre
+			if (games%10000 === 0) {
+				console.log('games: ', games, 'sequence', sequence);
+				var a = games; var b=0;
+				setTimeout(function(){
+					gps = games-a;
+					console.log('gier na sekunde: ', gps);
+				}, 1000);
+			}
+
+			games++;
+			//document.querySelector('#moves div').textContent = games;
+			sequence = nextSeq(sequence);
+			while(
+				reg.test(sequence) ||
+				sequence.indexOf('0') === -1 ||
+				sequence.indexOf('1') === -1 ||
+				sequence.indexOf('2') === -1 ||
+				sequence.indexOf('3') === -1 ||
+				sequence.indexOf('4') === -1 ||
+				sequence.indexOf('5') === -1 ||
+				sequence.indexOf('6') === -1
+			) {
+				sequence = nextSeq(sequence);
+				if (sequence === maxSequence) {
+					console.log('koncowa sekwencja = ', sequence);
+					alert('sprawdzilem wsio');
+					return;
+				}
+			}
+			currentMove = 0;
+			moves = 0;
+			board = JSON.parse(JSON.stringify(cachedBoard));
+			//draw();
+			autoPlay();
+		}
+	} else {
+		//wygralem!!
+		console.log('wygralem!');
+		currentMove++;
+		if (typeof window !== 'undefined') {
+			document.getElementById('history').innerHTML += 'Wygralem w ' + currentMove +
+			 ' ruchach. Sekwencja: ' + sequence + '<br/><br/>';
+		}
+
+		console.log('Wygralem w ' + currentMove +
+		 ' ruchach. Sekwencja: ' + sequence);
+
+		sendmsg = false;
+		sendHCMsg('@all Got it!!! Winning sequence: ' + sequence);
+		// games++;
+		// if (moves < lowest) {
+		// 	lowest = moves;
+		// 	moves = '<span style="color: #f00">' + moves + '</span>';
+		// 	console.log('Lowest:', lowest);
+		// } else if (moves > highest) {
+		// 	highest = moves;
+		// 	moves = '<span style="color: #00f">' + moves + '</span>';
+		// 	console.log('Highest:', highest);
+		// }
+		// document.querySelector('#moves div').textContent = games;
+		//
+		// document.getElementById('history').innerHTML += moves + ', ';
+		// document.getElementById('history').scrollTop = 1000000000;
+		// moves = 0;
+		// //generateBoard();
+		// board = JSON.parse(JSON.stringify(cachedBoard));
+		// draw();
+		// autoPlay();
+	}
+};
+go();
